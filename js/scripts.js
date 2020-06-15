897ba6daa12c5d07b0538ea075ff51c309fb34ea
suejohnmo <susan.mwaura@gmail.com>
2020-06-15 15:53:36 +0300

    modifying script file

--- js/scripts.js
+++ js/scripts.js
@@ -100,7 +100,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $("form#form34A").submit(function (event) {
+  $("form#clientForm").submit(function (event) {
     // event.preventDefault();
     var name = $("input#MERGE1").val();
     var email = $("input#MERGE0").val();
